32a70264d48998464f5c3a067307ebe6bad11398
Jiri Hegar <jiri.hegar@trustlab.cz>
2024-10-16 19:57:54 +0200

    a4

--- src/main/java/com/hexar/springhibernate/service/HelpService.java
+++ src/main/java/com/hexar/springhibernate/service/HelpService.java
@@ -15,7 +15,7 @@ public class HelpService {
         int count = persons.size();
 
 
-         //dev test test  a1 a2 a3
+         //dev test test  a1 a2 a3 a4
         // Vypsání počtu do konzole
         System.out.println("Počet osob: " + count);
     }
